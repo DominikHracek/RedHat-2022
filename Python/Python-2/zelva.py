af59dab3d4e6255bf146a1472e418ae673775991
Dominik Hráček <domca.hracek@gmail.com>
2022-05-24 13:27:59 +0200

    Python

--- Python/Python-2/zelva.py
+++ Python/Python-2/zelva.py
@@ -1,29 +1,17 @@
-from turtle import forward, left, exitonclick, shape
+from turtle import forward, left, right, exitonclick, shape
 
 shape('turtle')
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(110)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(110)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(90)
-forward(50)
-left(50)
+right(120)
+for j in range(6):
+    for i in range(3):
+        forward(30)
+        left(60)
+    right(120)
 
+left(120)
+for k in range(6):
+    for m in range(3):
+        forward(30)
+        right(60)
+    right(120)
 exitonclick()
